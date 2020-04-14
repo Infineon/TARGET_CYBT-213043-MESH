78603a52a81f27f0e1134dcd1a218534b6b84af5
gitlab-runner <do-not-reply@cypress.com>
2020-04-14 11:52:30 -0500

    Upload TARGET_CYBT-213043-MESH 2.4.0.6698

--- COMPONENT_bsp_design_modus/GeneratedSource/cycfg_pins.h
+++ COMPONENT_bsp_design_modus/GeneratedSource/cycfg_pins.h
@@ -4,9 +4,13 @@
 * Description:
 * Pin configuration
 * This file was automatically generated and should not be modified.
+* Tools Package 2.1.0.1266
+* 20819A1
+* personalities 1.0.0.31
+* udd 1.2.0.128
 *
 ********************************************************************************
-* Copyright 2017-2019 Cypress Semiconductor Corporation
+* Copyright 2020 Cypress Semiconductor Corporation
 * SPDX-License-Identifier: Apache-2.0
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -84,7 +88,6 @@ extern "C" {
 #define CYBSP_THERM_REF_INPUT_ENABLED 1U
 #define CYBSP_THERM_REF_INPUT WICED_P09
 #define CYBSP_SPI_FLASH_ENABLED 1U
-#define CYBSP_UART_ENABLED 1U
 #define CYBSP_PUART_ENABLED 1U
 
 extern const wiced_platform_gpio_t platform_gpio_pins[];
